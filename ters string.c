73322d28def9061f9122db3a6ba952c3bc623fbf
KeremOzcan123 <139489631+KeremOzcan123@users.noreply.github.com>
2023-08-19 13:35:52 +0300

    Add files via upload

---
+++ ters string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ char duz_yazi[100]="Merhaba arkadaslar nasilsiniz?";
+ int i;
+ 
+ int main(){
+ 
+ char dizi[30]="Merhaba arkadaslar nasilsiniz?";
+ 	
+ 	for(i=0;i<30;i++){
+printf("%c",dizi[29-i]);	 
+	 
+	 
+	}
+printf("\n");
+
+return 0;
+} 
+
